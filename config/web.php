23487688172369d23bbdde50c720da9c93d4a706
zhangjett <zhangjett@outlook.com>
2017-01-11 09:35:54 +0800

    udpate

--- config/web.php
+++ config/web.php
@@ -73,6 +73,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['*', '127.0.0.1', '::1']
     ];
 
     $config['bootstrap'][] = 'gii';
