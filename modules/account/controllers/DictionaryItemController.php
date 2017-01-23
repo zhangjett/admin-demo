84c0235fbf3ecd5794da796eb7391723b6c1992f
zhangjett <zhangjett@outlook.com>
2017-01-23 21:14:42 +0800

    update

--- modules/account/controllers/DictionaryItemController.php
+++ modules/account/controllers/DictionaryItemController.php
@@ -115,6 +115,7 @@ class DictionaryItemController extends Controller
         echo $this->renderPartial("update", ['model' => $model]);
         Yii::$app->end();
 
+        
         return 0;
     }
 }
