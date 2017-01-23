00b38a8dcaa1ca8592dd465526f3f82626cafc8f
zhangjett <zhangjett@outlook.com>
2017-01-23 23:03:11 +0800

    update

--- modules/account/views/operator/upload.php
+++ modules/account/views/operator/upload.php
@@ -118,6 +118,7 @@ $this->title = '上传图片';
             },
             onError: function(id, name, errorReason, xhrOrXdr) {
                 layer.msg(errorReason, {icon: 5});
+                
             }
         }
     });
