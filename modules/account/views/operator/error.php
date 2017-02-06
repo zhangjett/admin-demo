1a43744263912686af4da2f3a234d5e64e182b42
zhangjett <zhangjett@outlook.com>
2017-02-06 11:19:11 +0800

    update

--- modules/account/views/operator/error.php
+++ modules/account/views/operator/error.php
@@ -15,17 +15,8 @@ $this->title = $name;
 </div>
 <div class="modal-body">
     <div class="error-page">
-        <h2 class="text-yellow"> <?= Html::encode($this->title) ?></h2>
-
         <div>
             <h3><i class="fa fa-warning text-yellow"></i> Oops! <?= nl2br(Html::encode($message)) ?></h3>
-
-            <p>
-                The above error occurred while the Web server was processing your request.
-            </p>
-            <p>
-                Please contact us if you think this is a server error. Thank you.
-            </p>
         </div>
         <!-- /.error-content -->
     </div>
